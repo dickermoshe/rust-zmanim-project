dc0da74f19384627afe361bb1db2111e00c6585e
Moshe Dicker <dickermoshe@gmail.com>
2026-01-14 08:56:13 -0500

    fix test

--- crates/limudim-calendar/src/amud_yomi_bavli_dirshu.rs
+++ crates/limudim-calendar/src/amud_yomi_bavli_dirshu.rs
@@ -221,7 +221,7 @@ mod tests {
     }
     #[test]
     fn amud_yomi_bavli_2025_23_9() {
-        let test_date = from_gregorian_date(2025, 2, 3);
+        let test_date = from_gregorian_date(2025, 9, 23);
         let limud = AmudYomiBavliDirshu::default().limud(test_date).expect("limud exists");
         assert_eq!(limud.page, 34);
         assert_eq!(limud.side, Side::Aleph);
